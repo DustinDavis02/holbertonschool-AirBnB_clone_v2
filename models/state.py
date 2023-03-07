fd244e7621bf069d82b4068228d7fef948cca218
Dustin Davis <davisdustin328@gmail.com>
2023-03-07 14:38:21 -0600

    Default file.

--- models/state.py
+++ models/state.py
@@ -1,23 +1,8 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-from models.city import City
-from models.base_model import Base
 
 
-class State(BaseModel, Base):
+class State(BaseModel):
     """ State class """
-    __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-    if models.storage_type == 'db':
-        cities = relationship("City", backref="state", cascade="all, delete")
-    else:
-        @property
-        def cities(self):
-            city_list = []
-            for city in list(models.storage.all(City).values()):
-                if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list
+    name = ""
