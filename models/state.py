747325f9146179bfa1519af0c2e84f2fa848be54
Dustin Davis <davisdustin328@gmail.com>
2023-03-07 16:40:21 -0600

    Making city and state SQLAlchemy compatable

--- models/state.py
+++ models/state.py
@@ -1,26 +1,32 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String
 import models
+from models.city import City
+import shlex
 
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = "states"
+    __tablename__='states'
     name = Column(String(128), nullable=False)
-    
-    if models.storage_type == "db":
-        cities = relationship("City", cascade="all, delete", backref="state")
-    else:
-        @property
-        def cities(self):
-            """getter attribute that returns the list of City instances"""
-            from models.city import City
-            city_list = []
-            all_cities = models.storage.all(City)
-            for city in all_cities.values():
-                if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list
+    cities = relationship("City", cascade='all, delete, delete-orphan',
+                        backref="state")
+
+    @property
+    def cities(self):
+        var = models.storage.all()
+        lista = []
+        result = []
+        for key in var:
+            city = key.replace('.', ' ')
+            city = shlex.split(city)
+            if (city[0] == 'City'):
+                lista.append(var[key])
+        for elem in lista:
+            if (elem.state_id == self.id):
+                result.append(elem)
+        return (result)
