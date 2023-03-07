6f5059c5b5fa734312c7bba86f3443a6dc439d36
Dustin Davis <davisdustin328@gmail.com>
2023-03-07 13:49:33 -0600

    Return new ID

--- console.py
+++ console.py
@@ -116,33 +116,31 @@ class HBNBCommand(cmd.Cmd):
     def do_create(self, arg):
         """Creates a new instance of a class and saves it (to the JSON file)
         Usage: create <class name>"""
-        if not arg:
+        if not args:
             print("** class name missing **")
             return
-        args = arg.split()
-        if args[0] not in self.classes:
+        elif args.split()[0] not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-        new_instance = self.classes[args[0]]()
-        for i in range(1, len(args)):
-            if "=" not in args[i]:
-                continue
-            att_name, value = args[i].split("=", 1)
-            if not value:
-                continue
-            if value[0] == '"' and value[-1] == '"' and len(value) > 1:
-                value = value[1:-1].replace("_", " ").replace('"', '\\"')
-            elif "." in value:
-                try:
-                    value = float(value)
-                except ValueError:
-                    continue
+
+        new_instance = HBNBCommand.classes[args.split()[0]]()
+
+        for arg in args.split()[1:]:
+            key_value = arg.partition("=")
+            key = key_value[0]
+            value = key_value[2]
+
+            if value[0] == '"' and value[-1] == '"':
+                value = value[1:-1].replace('_', ' ')
+            elif '.' in value:
+                value = float(value)
             else:
-                try:
-                    value = int(value)
-                except ValueError:
-                    continue
-            setattr(new_instance, att_name, value)
+                value = int(value)
+            setattr(new_instance, key, value)
+
+        storage.new(new_instance)
+        storage.save()
+
         print(new_instance.id)
 
     def help_create(self):
