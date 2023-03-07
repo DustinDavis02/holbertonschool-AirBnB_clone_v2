83c1d7cef6a463755b7c5d043c7cbaeebc3357d2
Dustin Davis <davisdustin328@gmail.com>
2023-03-07 16:18:02 -0600

    Create instance of DBStorage

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
+"""This module instantiates an object of class depending on the value of HBNB_TYPE_STORAGE"""
+import os
 from models.engine.file_storage import FileStorage
+from models.engine.db_storage import DBStorage
 
 
-storage = FileStorage()
+if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+    storage = DBStorage()
+else:
+    storage = FileStorage()
+
 storage.reload()
+
