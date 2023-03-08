a441928b9ae517bcb8ac35a299ab7b155225f597
Dustin Davis <davisdustin328@gmail.com>
2023-03-08 07:23:26 -0600

    Adding defualt files back

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,24 +1,21 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
+
+
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
     __file_path = 'file.json'
     __objects = {}
-    def all(self, cls=None):
+
+    def all(self):
         """Returns a dictionary of models currently in storage"""
-        if cls is not None:
-            if type(cls) is str:
-                cls = eval(cls)
-            spec_rich = {}
-            for ky, vl in self.__objects.items():
-                if cls == type(vl):
-                    spec_rich[ky] = vl
-            return spec_rich
-        return self.__objects
+        return FileStorage.__objects
+
     def new(self, obj):
         """Adds new object to storage dictionary"""
         self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
+
     def save(self):
         """Saves storage dictionary to file"""
         with open(FileStorage.__file_path, 'w') as f:
@@ -27,37 +24,27 @@ class FileStorage:
             for key, val in temp.items():
                 temp[key] = val.to_dict()
             json.dump(temp, f)
-    def delete(self, obj=None):
-        """Delete obj From dictionary"""
-        if obj is None:
-            return
-        key_val = "{}.{}".format(type(obj).__name__, obj.id)
-        if FileStorage.__objects.get(key_val) is not None:
-            del FileStorage.__objects[key_val]
-        self.save()
-
 
     def reload(self):
         """Loads storage dictionary from file"""
-def reload(self):
-    """Loads storage dictionary from file"""
-    from models.base_model import BaseModel
-    from models.user import User
-    from models.place import Place
-    from models.state import State
-    from models.city import City
-    from models.amenity import Amenity
-    from models.review import Review
-    classes = {
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.place import Place
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.review import Review
+
+        classes = {
                     'BaseModel': BaseModel, 'User': User, 'Place': Place,
                     'State': State, 'City': City, 'Amenity': Amenity,
                     'Review': Review
                   }
-    try:
-        temp = {}
-        with open(FileStorage.__file_path, 'r') as f:
-            temp = json.load(f)
-            for key, val in temp.items():
-                self.all()[key] = classes[val['__class__']](**val)
-    except FileNotFoundError:
-        pass
+        try:
+            temp = {}
+            with open(FileStorage.__file_path, 'r') as f:
+                temp = json.load(f)
+                for key, val in temp.items():
+                        self.all()[key] = classes[val['__class__']](**val)
+        except FileNotFoundError:
+            pass
