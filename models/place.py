1a20302bc3eb0135f57f31bfb28eb186b6409ec8
Dustin Davis <davisdustin328@gmail.com>
2023-03-07 21:27:22 -0600

    Adding class attribute review

--- models/place.py
+++ models/place.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 """This module defines a class Place"""
+import shlex
+from sqlalchemy.ext.declarative import declarative_base
+from os import getenv
+import models
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, Integer, String, Float, ForeignKey
@@ -20,3 +24,24 @@ class Place(BaseModel, Base):
     latitude = Column(Float)
     longitude = Column(Float)
     amenity_ids = []
+    
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        reviews = relationship("Review", cascade='all, delete, delete-orphan',
+                            backref="place")
+
+    else:
+        @property
+        def reviews(self):
+            """ Returns list of reviews.id """
+            var = models.storage.all()
+            lista = []
+            result = []
+            for key in var:
+                review = key.replace('.', ' ')
+                review = shlex.split(review)
+                if (review[0] == 'Review'):
+                    lista.append(var[key])
+            for elem in lista:
+                if (elem.place_id == self.id):
+                    result.append(elem)
+            return (result)
